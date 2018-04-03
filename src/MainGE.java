bbd992fe98f63e0abdf2881644cfec65a3ed4229
Emanuel <emanuel.fuetsch@hyperisland.se>
2018-04-03 20:43:38 +0200

    Updated version of MainGE with 5 jpanels.

--- src/MainGE.java
+++ src/MainGE.java
@@ -5,23 +5,37 @@ import javax.swing.*;
 public class MainGE extends JFrame {
 
 		JTextField field = new JTextField(8);
-		JLabel greet = new JLabel();
+		JLabel greet = new JLabel(" ");
 	
 	public MainGE() {
-		
 		super("Hello.");
-		setLayout(new FlowLayout());
-		add(new JLabel("Name:"));
-		add(field);
-		JButton button = new JButton("Greet");
-		button.addActionListener(ave -> greet.setText("Hello " + field.getText() + "! "));
-		add(button);
-		add(greet);
-		setSize(400, 100);
+		
+		JPanel north = new JPanel();
+		north.add(new JLabel("North"));
+		add(north, BorderLayout.NORTH);
+		
+		JPanel east = new JPanel();
+		JButton b1 = new JButton("Greet");
+		east.add(b1);
+		add(east, BorderLayout.EAST);
+		b1.addActionListener(ave -> greet.setText("Hello " + field.getText() + "! "));
+		
+		JPanel south = new JPanel();
+		south.add(greet);
+		add(south, BorderLayout.SOUTH);
+		
+		JPanel west = new JPanel();
+		JLabel jl1= new JLabel("Name:");
+		west.add(jl1);
+		add(west, BorderLayout.WEST);
+		
+		JPanel center = new JPanel();
+		center.add(field);
+		add(center, BorderLayout.CENTER);
+		
+		setSize(500, 200);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
-
-
