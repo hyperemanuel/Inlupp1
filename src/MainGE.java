39469b1c83a43226ad90343116810e762a7bdcc7
Emanuel <emanuel.fuetsch@hyperisland.se>
2018-04-03 13:05:59 +0200

    The lambda way of implementing an ActionLstener using an anonymous class

--- src/MainGE.java
+++ src/MainGE.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MainGE extends JFrame implements ActionListener {
+public class MainGE extends JFrame {
 
 		JTextField field = new JTextField(8);
 		JLabel greet = new JLabel();
@@ -14,18 +14,13 @@ public class MainGE extends JFrame implements ActionListener {
 		add(new JLabel("Name:"));
 		add(field);
 		JButton button = new JButton("Greet");
-		button.addActionListener(this);
+		button.addActionListener(ave -> greet.setText("Hello " + field.getText() + "! "));
 		add(button);
 		add(greet);
 		setSize(400, 100);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
-	public void actionPerformed(ActionEvent ave) {
-		String name = field.getText();
-		greet.setText("Hello " + name + "! ");
 	}
 }
 
