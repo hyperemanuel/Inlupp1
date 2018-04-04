66b336e92d87bbe7109c967d3c09885ef3cbd637
Emanuel <emanuel.fuetsch@hyperisland.se>
2018-04-04 23:37:18 +0200

    First leg of MainGE skeleton done.

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,28 @@
 public class Main {
 
 	public static void main(String[] args) {
-		 Jewellery j1 = new Jewellery("Necklace", 4, true);
-		 Jewellery j2 = new Jewellery("Ring", 15, true);
-		 Jewellery j3 = new Jewellery("Ring", 15, false);
-		 
-		 Stock s1 = new Stock("Apple", 500, 100);
-		 Stock s2 = new Stock("DOW", 30, 80);
-		 Stock s3 = new Stock("BTC", 2, 7000);
-		 
-		 Apparatus a1 = new Apparatus("iPhone", 500, 7);
-		 Apparatus a2 = new Apparatus("MBP", 2000, 8);
-		 Apparatus a3 = new Apparatus("ThinkPad", 4500, 10);
-		 
-		 System.out.println(j1);
-		 System.out.println(j2);
-		 System.out.println(j3);
-		 System.out.println(s1);
-		 System.out.println(s2);
-		 System.out.println(s3);
-		 System.out.println(a1);
-		 System.out.println(a2);
-		 System.out.println(a3);
-		 
+//		 Jewellery j1 = new Jewellery("Necklace", 4, true);
+//		 Jewellery j2 = new Jewellery("Ring", 15, true);
+//		 Jewellery j3 = new Jewellery("Ring", 15, false);
+//		 
+//		 Stock s1 = new Stock("Apple", 500, 100);
+//		 Stock s2 = new Stock("DOW", 30, 80);
+//		 Stock s3 = new Stock("BTC", 2, 7000);
+//		 
+//		 Apparatus a1 = new Apparatus("iPhone", 500, 7);
+//		 Apparatus a2 = new Apparatus("MBP", 2000, 8);
+//		 Apparatus a3 = new Apparatus("ThinkPad", 4500, 10);
+//		 
+//		 System.out.println(j1);
+//		 System.out.println(j2);
+//		 System.out.println(j3);
+//		 System.out.println(s1);
+//		 System.out.println(s2);
+//		 System.out.println(s3);
+//		 System.out.println(a1);
+//		 System.out.println(a2);
+//		 System.out.println(a3);
+//		 
 		 new MainGE();
 	}
 }
--- src/MainGE.java
+++ src/MainGE.java
@@ -1,41 +1,59 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class MainGE extends JFrame {
 
-		JTextField field = new JTextField(8);
-		JLabel greet = new JLabel(" ");
+		JTextArea textarea = new JTextArea();
 	
 	public MainGE() {
 		super("Hello.");
 		
 		JPanel north = new JPanel();
-		north.add(new JLabel("North"));
+		north.add(new JLabel("Valuable"));
 		add(north, BorderLayout.NORTH);
 		
 		JPanel east = new JPanel();
-		JButton b1 = new JButton("Greet");
-		east.add(b1);
+		east.setLayout(new BoxLayout(east, BoxLayout.Y_AXIS));
+		east.setBorder(new EmptyBorder(350, 10, 10, 10));
+		east.add(new JLabel("Group by:"));
+		ButtonGroup b1 = new ButtonGroup();
+		JRadioButton radioName = new JRadioButton("Name");
+		JRadioButton radioValue = new JRadioButton("Value");
+		b1.add(radioName);
+		b1.add(radioValue);
+		east.add(radioName);
+		east.add(radioValue);
 		add(east, BorderLayout.EAST);
-		b1.addActionListener(ave -> greet.setText("Hello " + field.getText() + "! "));
 		
 		JPanel south = new JPanel();
-		south.add(greet);
+		JLabel newItem = new JLabel("New:");
+		south.add(newItem);
+		JComboBox<String> c1 = new JComboBox<>();
+		c1.addItem("Jewellery");
+		c1.addItem("Stock");
+		c1.addItem("Apparatus");
+		south.add(c1);
+		JButton show= new JButton("Show");
+		south.add(show);
+		JButton mCrash = new JButton("Market crash");
+		south.add(mCrash);
 		add(south, BorderLayout.SOUTH);
 		
-		JPanel west = new JPanel();
-		JLabel jl1= new JLabel("Name:");
-		west.add(jl1);
-		add(west, BorderLayout.WEST);
+		textarea.setText(addText());
+		textarea.setEditable(false);
+		JScrollPane scroll = new JScrollPane(textarea);
+		add(scroll, BorderLayout.CENTER);
 		
-		JPanel center = new JPanel();
-		center.add(field);
-		add(center, BorderLayout.CENTER);
-		
-		setSize(500, 200);
+		setSize(1000, 500);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private String addText() {
+		String str = "jkhgsdjkghsjkhgjkshgjkshjks\nhkjsjkhskjhsl\nkjhlskfjhlkfsjh\nlkfsjhk\nlsjbsmwrohmbsopaöbjkhgsdjkghsjkhgjkshgjkshjks\nhkjsjkhskjhsl\nkjhlskfjhlkfsjh\nlkfsjhk\nlsjbsmwrohmbsopaöbjkhgsdjkghsjkhgjkshgjkshjks\nhkjsjkhskjhsl\nkjhlskfjhlkfsjh\nlkfsjhk\nlsjbsmwrohmbsopaöbjkhgsdjkghsjkhgjkshgjkshjks\nhkjsjkhskjhsl\nkjhlskfjhlkfsjh\nlkfsjhk\nlsjbsmwrohmbsopaöbjkhgsdjkghsjkhgjkshgjkshjks\nhkjsjkhskjhsl\nkjhlskfjhlkfsjh\nlkfsjhk\nlsjbsmwrohmbsopaöbjkhgsdjkghsjkhgjkshgjkshjks\nhkjsjkhskjhsl\nkjhlskfjhlkfsjh\nlkfsjhk\nlsjbsmwrohmbsopaöb";
+		return str;
 	}
 }
