07bf3557a9f1ba7acef17c7cc30620482785ab55
Emanuel <emanuel.fuetsch@hyperisland.se>
2018-04-09 23:17:51 +0200

    Forms 1 draft.

--- src/MainGE.java
+++ src/MainGE.java
@@ -6,6 +6,8 @@ import javax.swing.border.EmptyBorder;
 public class MainGE extends JFrame {
 
 		JTextArea textarea = new JTextArea();
+		String[] options = {"Select category..", "Jewellery", "Stock", "Valuable"};
+		JComboBox<String> c1 = new JComboBox<String>(options);
 	
 	public MainGE() {
 		super("Hello.");
@@ -30,11 +32,6 @@ public class MainGE extends JFrame {
 		JPanel south = new JPanel();
 		JLabel newItem = new JLabel("New:");
 		south.add(newItem);
-		JComboBox<String> c1 = new JComboBox<>();
-		c1.addItem("Select category..");
-		c1.addItem("Jewellery");
-		c1.addItem("Stock");
-		c1.addItem("Apparatus");
 		south.add(c1);
 		JButton show= new JButton("Show");
 		south.add(show);
@@ -52,7 +49,104 @@ public class MainGE extends JFrame {
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	
-	
 }
+
+	class jewelleryListener implements ActionListener{
+		public void actionPerformed(ActionEvent ave) {
+			
+		}
+	}
+
+	class JForm extends JPanel{
+		JTextField jewName = new JTextField(10);
+		JTextField gemstones = new JTextField(3);
+		JCheckBox goldenBox = new JCheckBox("Gold");
+		
+		JForm(){
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			JPanel r1 = new JPanel();
+			r1.add(new JLabel("Name:"));
+			r1.add(jewName);
+			add(r1);
+			JPanel r2 = new JPanel();
+			r2.add(new JLabel("Gemstones:"));
+			r2.add(gemstones);
+			add(r2);
+			add(goldenBox);
+		}
+		public String getName() {
+			return jewName.getText();
+		}
+		public int getGemstones() {
+			return Integer.parseInt(gemstones.getText());
+		}
+		public boolean getGolden() {
+			return goldenBox.isSelected();
+		}
+}
+	
+	class stockListener implements ActionListener{
+		public void actionPerformed(ActionEvent ave) {
+			
+		}
+	}
+	
+	class SForm extends JPanel{
+		JTextField stockName = new JTextField(15);
+		JTextField amount = new JTextField(3);
+		JTextField price = new JTextField(4);
+		
+		SForm(){
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			JPanel r1 =new JPanel();
+			r1.add(new JLabel("Name:"));
+			r1.add(stockName);
+			add(r1);
+			JPanel r2 = new JPanel();
+			r2.add(new JLabel("Amount:"));
+			r2.add(amount);
+			add(r2);
+			JPanel r3 = new JPanel();
+			r3.add(new JLabel("Price:"));
+			r3.add(price);
+			add(r3);
+		}
+		
+		public String getName() {
+			return stockName.getText();
+		}
+		
+		public int getAmount() {
+			return Integer.parseInt(amount.getText());
+		}
+		
+		public int getPrice() {
+			return Integer.parseInt(price.getText());
+		}
+	}
+	
+	class AForm extends JPanel{
+		JTextField aName = new JTextField(15);
+		JTextField price = new JTextField(4);
+		JTextField wear = new JTextField(2);
+		
+		AForm(){
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			JPanel r1 =new JPanel();
+			r1.add(new JLabel("Name:"));
+			r1.add(aName);
+			add(r1);
+			JPanel r2 = new JPanel();
+			r2.add(new JLabel("Amount:"));
+			r2.add(price);
+			add(r2);
+			JPanel r3 = new JPanel();
+			r3.add(new JLabel("Price:"));
+			r3.add(wear);
+			add(r3);
+		}
+		
+	}
+	
+	
+	
